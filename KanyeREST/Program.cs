f1e0a3b7c042a28579fbdb71240fea12d580f0fb
Monica Crowe <mcrowe9194@gmail.com>
2022-06-28 19:01:28 -0500

    Add project files.

---
+++ KanyeREST/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeREST
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                for (int i = 0; i < 5; i++)
+                {
+                    QuoteGenerator.KanyeQuote();
+                    Console.WriteLine();
+                    QuoteGenerator.RonQuote();
+                    Console.WriteLine();
+                    Thread.Sleep(1000);
+                }
+            
+        }
+    }
+}
