22ee7d29effad118063f61a748ed19bc54e1cd2a
Monica Crowe <mcrowe9194@gmail.com>
2023-01-31 09:18:09 -0600

    Added comments explaining what everything does

--- KanyeREST/QuoteGenerator.cs
+++ KanyeREST/QuoteGenerator.cs
@@ -8,28 +8,28 @@ namespace KanyeREST
     {
         public static void KanyeQuote()
         {
-            var client = new HttpClient();
+            var client = new HttpClient(); //http request
 
-            var kanyeURL = "https://api.kanye.rest";
+            var kanyeURL = "https://api.kanye.rest"; //hitting the api 
 
-            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result; //getting result of api, aspecifically 
 
-            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString(); //parsing JSON OBJECT from response and turning it into a string 
 
-            Console.WriteLine($"Kanye: \"{kanyeQuote}\"");
+            Console.WriteLine($"Kanye: \"{kanyeQuote}\""); //quote line, prints to console
         }
 
         public static void RonQuote()
         {
-            var client = new HttpClient();
+            var client = new HttpClient(); //http request
 
-            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"; //hitting the api
 
-            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronResponse = client.GetStringAsync(ronURL).Result; //getting result of api, aspecifically 
 
-            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim(); //parsing JSON ARRAY from response and turning it into a string
 
-            Console.WriteLine($"Ron: {ronQuote}");
+            Console.WriteLine($"Ron: {ronQuote}"); //quote line, prints to console
         }
 
 
